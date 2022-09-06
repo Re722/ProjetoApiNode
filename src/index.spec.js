20ce992db6453f07611e3f4c61700fe12b2133b8
Renaldo Stammerjohann <renaldo@pastoraldacrianca.org.br>
2022-09-06 15:31:23 -0300

    testing the route

--- src/index.spec.js
+++ src/index.spec.js
@@ -1,20 +1,10 @@
-const request = require("supertest")
+const request = require("supertest");
+const UserController = require("./controller/UserController");
 const app = require('./routes')
 
 test('testando a rota da aplicacao', () => {
   const response = request(app)
-    .get('./');
+    .get('/list-categoria');
 
-  expect(response.statusCode).toEqual("200");
-  expect(response.body).toHaveProperty('HELLO');
 
-})
-
-// describe("Testando minha aplicacao", () => {
-//   it("should get main route", async () => {
-//     const res = await request(app).get('/')
-
-//     expect(res.statusCode).toEqual('200')
-//     expect(res.body).toHaveProperty('HELLO')
-//   })
-// })
+ })
